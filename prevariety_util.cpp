256f6232ab44522e93fbc67222a2aa35d01a91b5
Jeff Sommars <sommars1@uic.edu>
2017-08-20 06:39:36 -0500

    Close file

--- prevariety_util.cpp
+++ prevariety_util.cpp
@@ -123,6 +123,7 @@ vector<vector<vector<int> > > ParseSupportFile(string &FileName)
 {
    ifstream t(FileName);
    string str((std::istreambuf_iterator<char>(t)),std::istreambuf_iterator<char>());
+   t.close();
    str.erase(std::remove(str.begin(), str.end(), '\n'), str.end());
    str = "[" + str + "]";
    return ParseToSupport(str);
